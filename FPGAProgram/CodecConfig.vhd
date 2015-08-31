ed5b67076a2cf7c40f32723330fe8b785684f190
itachihellder <helder.garjunior@outlook.com>
2015-08-31 20:44:43 -0300

    DA Config System

---
+++ FPGAProgram/CodecConfig.vhd
@@ -0,0 +1,15 @@
+--Codec system configuration i2c
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY CodecConfig is 
+	port(
+		
+	);
+end CodecConfig;
+
+ARCHITECTURE archconfig of CodecConfig is
+	begin
+	
+end archconfig;
